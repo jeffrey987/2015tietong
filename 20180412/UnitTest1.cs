8afb834df75581e5d73ce5c6a4c0e1af7fbb203b
jeffrey987 <921491129@qq.com>
2018-04-12 16:37:25 +0800

    20180412

--- 20180412/UnitTest1.cs
+++ 20180412/UnitTest1.cs
@@ -11,7 +11,24 @@ namespace _20180412
         {
             Console.WriteLine(GetSum(-1, 2));
         }
-
+        //public int GetSum(int a, int b)
+        //{
+        //    int length = Math.Abs(a - b);
+        //    int item = a > b ? b : a;
+        //    int result = 0;
+        //    for (int i = 0; i <= length; i++)
+        //    {
+        //        result += item;
+        //        item++;
+        //    }
+        //    return result;
+        //}
+        /// <summary>
+        /// Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int GetSum(int a, int b)
         {
             int temp = (Math.Abs(a - b) + 1) / 2;
