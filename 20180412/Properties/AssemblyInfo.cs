23f2d30ea5d6493726ea5e9e29b554c5f9bb024f
jeffrey987 <921491129@qq.com>
2018-04-12 16:23:28 +0800

    20180412

---
+++ 20180412/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("20180412")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("20180412")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8de999d5-2c00-4a94-b90d-290a474c5369")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 20180412/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _20180412
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void testGetSum()
+        {
+            Console.WriteLine(GetSum(-1, 2));
+        }
+
+        public int GetSum(int a, int b)
+        {
+            int temp = (Math.Abs(a - b) + 1) / 2;
+            return (a + b) * temp;
+        }
+    }
+}
